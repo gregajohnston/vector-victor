7c01cd9a6352b1fd63e7f91483fd571479bdc2f7
Greg Johnston <greg.a.johnston@gmail.com>
2016-04-06 15:16:06 -0400

    Wrote and tested 'vector_sub'

--- linear_algebra.py
+++ linear_algebra.py
@@ -1,3 +1,6 @@
+class ShapeError(Exception):
+    pass
+
 
 
 def shape(list_or_matrix):
@@ -6,6 +9,14 @@ def shape(list_or_matrix):
     return tuple((len(list_or_matrix), len(list_or_matrix[0])))
 
 def vector_add(first_matrix, second_matrix):
+    if len(first_matrix) != len(second_matrix):
+        raise ShapeError
+
+    if type(first_matrix[0]) is list and type(second_matrix[0]) is list:
+        for index in range(len(first_matrix[0])):
+            if len(first_matrix[index]) != len(second_matrix[index]):
+                raise ShapeError
+
     if type(first_matrix[0]) is not list:
         return [x + y for x, y in zip(first_matrix, second_matrix)]
 
@@ -15,6 +26,18 @@ def vector_add(first_matrix, second_matrix):
     # for index in range(len(return_value)):
     #     return_value[index] = sum(return_value[index])
     # return return_value
+
+def vector_sub(first_matrix, second_matrix):
+    if len(first_matrix) != len(second_matrix):
+        raise ShapeError
+
+    if type(first_matrix[0]) is list and type(second_matrix[0]) is list:
+        for index in range(len(first_matrix[0])):
+            if len(first_matrix[index]) != len(second_matrix[index]):
+                raise ShapeError
+
+    if type(first_matrix[0]) is not list:
+        return [x - y for x, y in zip(first_matrix, second_matrix)]
 
 
 def main():
