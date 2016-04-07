4265361dc9b864a1023ea9c62c54af120953c2d3
Greg Johnston <greg.a.johnston@gmail.com>
2016-04-06 23:26:11 -0400

    Refactored all basic methods except multiplication.

--- linear_algebra.py
+++ linear_algebra.py
@@ -1,73 +1,146 @@
+
+
 class ShapeError(Exception):
     pass
 
 
-def shape(matrix):
-    if type(matrix[0]) is not list:
-        return tuple((len(matrix),))
-    return tuple((len(matrix), len(matrix[0])))
+
+def shape(vector):
+
+    return tuple((len(vector),))
 
 
-def vector_add(first_matrix, second_matrix):
-    if len(first_matrix) != len(second_matrix):
+
+def vector_add(first_vector, second_vector):
+
+    if type(first_vector) and type(second_vector) is not list:
+            return list(first_vector) + list(second_vector)
+
+    if len(list(first_vector)) != len(list(second_vector)):
         raise ShapeError
 
-    if type(first_matrix[0]) is list and type(second_matrix[0]) is list:
-        count = 0
-        count = [count + index for index in range(len(first_matrix[0]))
-                    if len(first_matrix[index]) != len(second_matrix[index])]
-        if count > 0:
+    if type(first_vector[0]) is not list:
+        return [first_vector[index] + value for index, value in enumerate(second_vector)]
+
+
+
+def vector_sub(first_vector, second_vector):
+
+    if type(first_vector) and type(second_vector) is not list:
+            return first_vector - second_vector
+
+    if len(list(first_vector)) != len(list(second_vector)):
+        raise ShapeError
+
+    if type(first_vector[0]) is not list:
+        return [first_vector[index] - value for index, value in enumerate(second_vector)]
+
+
+
+def vector_sum(*args):
+
+    if len(args) == 1:
+        return args[0]
+
+    else:
+        return vector_add(args[-1], vector_sum(*args[:-1]))
+
+
+
+def dot(first_vector, second_vector):
+
+    if len(list(first_vector)) != len(list(second_vector)):
+        raise ShapeError
+
+    if type(first_vector[0]) is not list:
+        return sum([first_vector[index] * value for index, value in enumerate(second_vector)])
+
+    if type(first_matrix[0]) and type(second_matrix[0]) is list:
+        uneven_list_check = [index for index in range(len(first_matrix[0]))
+                            if len(first_matrix[index]) != len(second_matrix[index])]
+        if uneven_list_check != []:
             raise ShapeError
 
-    if type(first_matrix[0]) is not list:
-        return [x + y for x, y in zip(first_matrix, second_matrix)]
-
-    # return_value = [zip(x, y) for x, y in zip(first_matrix, second_matrix)]
-    # for index in range(len(return_value)):
-    #     a = list[return_value[index]]
-    # for index in range(len(return_value)):
-    #     return_value[index] = sum(return_value[index])
-    # return return_value
 
 
-def vector_sub(first_matrix, second_matrix):
-    if len(first_matrix) != len(second_matrix):
-        raise ShapeError
+def vector_multiply(vector, scalar):
 
-    if type(first_matrix[0]) is list and type(second_matrix[0]) is list:
-        for index in range(len(first_matrix[0])):
-            if len(first_matrix[index]) != len(second_matrix[index]):
-                raise ShapeError
-
-    if type(first_matrix[0]) is not list:
-        return [x - y for x, y in zip(first_matrix, second_matrix)]
-
-
-def vector_sum(first_matrix, *args):
-    return_value = first_matrix
-    for arg in args:
-        return_value = vector_add(return_value, arg)
+    return_value = vector[:]
+    #return_value[] = [matrix[index] * scalar for index in range(len(matrix))]
+    for index in range(len(vector)):
+        return_value[index] = vector[index] * scalar
     return return_value
 
 
-def dot(first_matrix, second_matrix):
-    if len(first_matrix) != len(second_matrix):
-        raise ShapeError
+    # total_vector = vector_add(first_vector, second_vector)
+    # total_vector = [vector_add(total_vector, arg) for arg in args]
+    # return total_vector
 
-    if type(first_matrix[0]) is list and type(second_matrix[0]) is list:
-        for index in range(len(first_matrix[0])):
-            if len(first_matrix[index]) != len(second_matrix[index]):
-                raise ShapeError
-
-    if type(first_matrix[0]) is not list:
-        return sum([x * y for x, y in zip(first_matrix, second_matrix)])
+    # for arg in args:
+    #     return_value = vector_add(return_value, arg)
+    #return_value = [vector_add(x,y) for x,y in zip(return_value, args)]
 
 
-def vector_multiply(matrix, scalar):
-    return_value = matrix[:]
-    for index in range(len(matrix)):
-        return_value[index] = matrix[index] * scalar
-    return return_value
+
+
+# def shape(matrix):
+#
+#     if type(matrix[0]) is not list:
+#         return tuple((len(matrix),))
+#
+#     return tuple((len(matrix), len(matrix[0])))
+#
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+# def vector_add(first_matrix, second_matrix):
+#     if type(first_matrix) and type(second_matrix) is not list:
+#             return first_matrix + second_matrix
+#
+#     if len(first_matrix) != len(second_matrix):
+#         raise ShapeError
+#
+#     if type(first_matrix[0]) is not list:
+#         return [x + y for x, y in zip(first_matrix, second_matrix)]
+#
+#     if type(first_matrix[0]) and type(second_matrix[0]) is list:
+#         uneven_list_check = [index for index in range(len(first_matrix[0]))
+#                             if len(first_matrix[index]) != len(second_matrix[index])]
+#         if uneven_list_check != []:
+#             raise ShapeError
+#
+####################################
+# def vector_sub(first_matrix, second_matrix):
+#     if len(first_matrix) != len(second_matrix):
+#         raise ShapeError
+#
+#     if type(first_matrix[0]) is not list:
+#         return [x - y for x, y in zip(first_matrix, second_matrix)]
+#
+#     if type(first_matrix[0]) and type(second_matrix[0]) is list:
+#         uneven_list_check = [index for index in range(len(first_matrix[0]))
+#                             if len(first_matrix[index]) != len(second_matrix[index])]
+#         if uneven_list_check != []:
+#             raise ShapeError
+#
+####################################
+# def vector_sum(first_matrix, *args):
+#     return_value = first_matrix[:]
+#     for arg in args:
+#         return_value = vector_add(return_value, arg)
+#     #return_value = [vector_add(x,y) for x,y in zip(return_value, args)]
+#     return return_value
+
+# def vector_multiply(matrix, scalar):
+#
+#     return_value = matrix[:]
+#     #return_value[] = [matrix[index] * scalar for index in range(len(matrix))]
+#     for index in range(len(matrix)):
+#         return_value[index] = matrix[index] * scalar
+#     return return_value
+
+
+
+
+
 
 
 def vector_sum_for_mean(first_matrix, *args):
